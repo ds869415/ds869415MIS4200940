4378fe79d8af88823a95cc36d4e07b1a12bb1757
Drew Sado <ds869415@ohio.edu>
2020-01-29 10:34:14 -0500

    Controller and Case added

---
+++ DAL/MIS4200Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ds869415MIS4200940.Models;
+
+namespace ds869415MIS4200940.DAL
+{
+    public class MIS4200Context : DbContext
+    {
+        public MIS4200Context() : base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+    }
+}
---
+++ Models/Orders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ds869415MIS4200940.Models
+{
+    public class Orders
+    {
+        [Key]
+        public int orderNum { get; set; }
+        public string description { get; set; }
+        public DateTime orderDate { get; set; }
+        public int customerID { get; set; }
+        public virtual Customer customer { get; set; }
+
+    }
+}
